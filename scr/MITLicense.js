bf04be81ba94af527fcc70b21e6e1c341c7762a1
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2019-06-02 15:16:11 -0300

    Update MITLicense.js

--- scr/MITLicense.js
+++ scr/MITLicense.js
@@ -20,7 +20,8 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
-/***********************************************************************/class MITLicense {
+/***********************************************************************/
+class MITLicense {
 	static TEXT = 0;
 	static HTML = 1;
 	static src = "https://opensource.org/licenses/MIT";
