b4a4976ac9ead4307cb41801f19d34a002f28371
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2019-06-22 21:16:45 -0300

    20190622 Update

--- scr/Translation.js
+++
@@ -1,7 +0,0 @@
-function loadTranslation(iframe_id,translation_file, is_file) {
-	document.writeln("<iframe id=\""+iframe_id+"\" width=\"1024\" height=\"768\" src=\""+(is_file?"file:///X:/":"http://")+translation_file+"\"></iframe>");
-	var iframe = document.getElementById(iframe_id);
-	var content = iframe.contentDocument || iframe.contentWindow.document; 
-	console.log(iframe.contentWindow.document);
-	console.log(document);
-}
